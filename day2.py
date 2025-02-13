2461ef6d76f40a197c1489097c961abc4c1c2ac3
k-jojo3011 <kjoan3011@gmail.com>
2025-02-13 13:21:37 +0300

    first python code

---
+++ day2.py
@@ -0,0 +1,10 @@
+CURRENT_YEAR = 2025
+
+name = input("what's your name? ")
+age = input ("How old are you? ")
+
+future_age = int(age) + 5 #int converted the age to an integer from a string 
+
+#strings 
+
+print("Hello "  +name + " You will be " + str(future_age) + " years old. ")
---
+++ hello.py
@@ -0,0 +1,9 @@
+#This is a single line comment
+""" this is a multi line comment 
+take note of it """
+
+name = input("Enter your name: ")
+age = input("what's your age?  ")
+color = input("Whats your favourite color: ")
+print("Hello "  +  name +  ", Your age is  " +  ", You like the color " + color + ".")
+
