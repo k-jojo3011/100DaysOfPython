feca7f3bb0b8ac48b3506fe97fcd38799962a7ee
k-jojo3011 <kjoan3011@gmail.com>
2025-02-18 09:41:10 +0300

    added a practice file

---
+++ practice.py
@@ -0,0 +1,25 @@
+#1.
+"""price =100
+total_price = price + 50
+print(total_price)
+"""
+#2 .string manipulation
+
+
+#list operations 
+''''favouritefoods =["Chips","burger"," pasta"]
+favouritefoods.append("scrambled eggs")
+favouritefoods.pop(1)
+print(favouritefoods)
+
+'''
+#dictionary challenge
+ccdict = {
+    "Kenya": "Nairobi",
+    "China": "Beijing",
+    "Eritrea":"Asmara"
+
+}
+print()
+ccdict.update({"USA":"Washington DC"})
+print(ccdict)
